a6e9f17fe0706d16e56e415c67895dc23f31a2a1
Alexandre <alexandre@jsc-trade?com>
2022-07-28 14:55:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser")
+const express = require('express')
+const cors = require('cors');
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.route('/*')
+    .get(function(req, res) {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+    });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
