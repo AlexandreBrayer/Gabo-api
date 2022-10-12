63d40572cebfea7b60c34508d7eba0c41f5508f1
Alexandre <alexandre@jsc-trade?com>
2022-10-12 10:53:08 +0200

    [ADD] Prettierrc

--- models/Stat.ts
+++ models/Stat.ts
@@ -2,41 +2,41 @@ import { Schema, model } from "mongoose";
 const StatsModel = new Schema<IStats>({
     games: {
         type: Number,
-        default: 0
+        default: 0,
     },
     wins: {
         type: Number,
-        default: 0
+        default: 0,
     },
     losses: {
         type: Number,
-        default: 0
+        default: 0,
     },
     gabo: {
         type: Number,
-        default: 0
+        default: 0,
     },
     lowpen: {
         type: Number,
-        default: 0
+        default: 0,
     },
     contreGabo: {
         type: Number,
-        default: 0
+        default: 0,
     },
     highpen: {
         type: Number,
-        default: 0
+        default: 0,
     },
     lowDownhill: {
         type: Number,
-        default: 0
+        default: 0,
     },
     highDownhill: {
         type: Number,
-        default: 0
-    }
-}).set('toJSON', {
-    virtuals: true
+        default: 0,
+    },
+}).set("toJSON", {
+    virtuals: true,
 });
-export default model('Stats', StatsModel);
+export default model("Stats", StatsModel);
